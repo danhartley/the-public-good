5c53262c25330f0bc55dc7e7efa4dd38e0070b61
Daniel Hartley <danhartleybcn@gmail.com>
2021-02-24 17:07:42 +0000

    carbon badge

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from 'styles/Home.module.css';
+import Carbonbadge from "react-carbonbadge"
 
 export default function Home() {
   return (
@@ -46,7 +47,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        footer
+        <Carbonbadge darkMode={true} />
       </footer>
     </div>
   )
