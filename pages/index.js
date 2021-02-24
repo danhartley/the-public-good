fe000f955bb48234e6b504ded2511928d588e80c
Daniel Hartley <danhartleybcn@gmail.com>
2021-02-24 12:52:19 +0000

    fix heading element hierarchy

--- pages/index.js
+++ pages/index.js
@@ -33,12 +33,12 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
+            <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
