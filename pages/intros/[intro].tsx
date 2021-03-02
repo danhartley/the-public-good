0ac793591387244c4ab75791122a1bb682dd2c9d
Daniel Hartley <danhartleybcn@gmail.com>
2021-03-02 07:33:29 +0000

    comment intros temlate

--- pages/intros/[intro].tsx
+++ pages/intros/[intro].tsx
@@ -1,41 +1,41 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
-import intros from 'pages/intros/intros.json';
+// import intros from 'pages/intros/intros.json';
 
-import Link from 'next/link';
-import Layout from 'components/layout/layout';
+// import Link from 'next/link';
+// import Layout from 'components/layout/layout';
 
-import styles from 'pages/intros/Intro.module.scss';
+// import styles from 'pages/intros/Intro.module.scss';
 
-const Intro = ({intro}) => {
+// const Intro = ({intro}) => {
 
-    const router = useRouter();
+//     const router = useRouter();
 
-    const query = router.query;
+//     const query = router.query;
 
-    return (
-        <Layout title="Intro" description={`${intro}`} header={intro}>            
-            <div class={styles.grid}>intro stuff…</div>            
-        </Layout>
-    )
-};
+//     return (
+//         <Layout title="Intro" description={`${intro}`} header={intro}>            
+//             <div class={styles.grid}>intro stuff…</div>            
+//         </Layout>
+//     )
+// };
 
-export default Intro;
+// export default Intro;
 
-export const getStaticProps = async ({params}) => {
-    return {
-        props: {
-            intro: params.intro
-        }
-    }
-};
+// export const getStaticProps = async ({params}) => {
+//     return {
+//         props: {
+//             intro: params.intro
+//         }
+//     }
+// };
 
-export const getStaticPaths = async () => {
+// export const getStaticPaths = async () => {
     
-    const paths = intros.list.map(intro => `/intros/${intro.slug}`);
+//     const paths = intros.list.map(intro => `/intros/${intro.slug}`);
 
-    return {
-        paths,
-        fallback: false
-    };
-};
+//     return {
+//         paths,
+//         fallback: false
+//     };
+// };
