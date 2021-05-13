3b1eaba8b172d26d3d4ce80210cabc5c1d4ce5b5
Daniel Hartley <danhartleybcn@gmail.com>
2021-05-13 10:15:02 +0100

    trailing slash redirect paths

--- next.config.js
+++ next.config.js
@@ -26,23 +26,23 @@ module.exports = {
     async redirects() {
       return [
         {
-          source: '/change',
-          destination: '/climate-crisis/the-trickiest-of-transitions',
+          source: '/change/',
+          destination: '/climate-crisis/the-trickiest-of-transitions/',
           permanent: true,
         },
         {
-          source: '/hydrogen',
-          destination: '/climate-crisis/hydrogen-a-singular-atom',
+          source: '/hydrogen/',
+          destination: '/climate-crisis/hydrogen-a-singular-atom/',
           permanent: true,
         },
         {
-          source: '/carbon',
-          destination: '/climate-crisis/carbon-component-of-life',
+          source: '/carbon/',
+          destination: '/climate-crisis/carbon-component-of-life/',
           permanent: true,
         },
         {
-          source: '/carbonfootprint',
-          destination: '/climate-crisis/the-value-of-individual-acts',
+          source: '/carbonfootprint/',
+          destination: '/climate-crisis/the-value-of-individual-acts/',
           permanent: true,
         },
       ]
