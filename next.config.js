a2b90f9e8d43f5bcd0186a5bcab611a014acbf34
Daniel Hartley <danhartleybcn@gmail.com>
2021-05-13 17:23:24 +0100

    no headers no rewrites

--- next.config.js
+++ next.config.js
@@ -24,32 +24,32 @@ module.exports = withPWA({
 
 // full redirect to new url
 
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/change',
-          destination: '/climate-crisis/the-trickiest-of-transitions',
-          permanent: true,
-        },
-        {
-          source: '/hydrogen',
-          destination: '/climate-crisis/hydrogen-a-singular-atom',
-          permanent: true,
-        },
-        {
-          source: '/carbon',
-          destination: '/climate-crisis/carbon-component-of-life',
-          permanent: true,
-        },
-        {
-          source: '/carbonfootprint',
-          destination: '/climate-crisis/the-value-of-individual-acts',
-          permanent: true,
-        },
-      ]
-    },
-}
+// module.exports = {
+//     async redirects() {
+//       return [
+//         {
+//           source: '/change',
+//           destination: '/climate-crisis/the-trickiest-of-transitions',
+//           permanent: true,
+//         },
+//         {
+//           source: '/hydrogen',
+//           destination: '/climate-crisis/hydrogen-a-singular-atom',
+//           permanent: true,
+//         },
+//         {
+//           source: '/carbon',
+//           destination: '/climate-crisis/carbon-component-of-life',
+//           permanent: true,
+//         },
+//         {
+//           source: '/carbonfootprint',
+//           destination: '/climate-crisis/the-value-of-individual-acts',
+//           permanent: true,
+//         },
+//       ]
+//     },
+// }
 
 // keep url but return new destination
 
