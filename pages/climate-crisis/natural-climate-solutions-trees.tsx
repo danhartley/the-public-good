c8ee581ad7e649879ab7da4bab6181560ee2c2c4
Daniel Hartley <danhartleybcn@gmail.com>
2021-11-09 09:56:27 +0000

    image for trees as natural climate solution

--- pages/climate-crisis/natural-climate-solutions-trees.tsx
+++ pages/climate-crisis/natural-climate-solutions-trees.tsx
@@ -107,7 +107,7 @@ const references = [
 
 const NCSPartOne = () => {
     return (
-        <Layout header="Natural climate solutions - Trees" rt="10-15" title="natural-climate-solutions-trees" description={'Trees'}>
+        <Layout header="Natural climate solutions - Trees" rt="10-15" title="natural-climate-solutions-trees" description={'Trees'} image="https://lh3.googleusercontent.com/xYJeSlYaHX6cb3Jv7V7HxYZDTmT6vQQT6BoLAeC-c2F8GH1O9F0Mz1x2e1KGLY1Qx8HlfCIU2GLbC4HqHwOEfWNqauMXesTRJHNRtZFPnOLX7IEPJnK7HhFjzHQ4RhwVsli_fynV-9yuZJkIyiv3TyNRNvp3S1nXZkccrszVcNzTDKlsK4DaOIXQpf6R0opkJxRQWfxRcrcCckA85H8MYwzsGD1-w-o0S6MXaIlaMH37wS1-pvnG-xWkwCUyVyFllTwXMhcFTxyRaHiuNYqfbXTfk4gfOwhgrvJHHEqch096v5nCxvguBtKgqQ0kgtobozubuKxM3-23oRrWlou2vDu5G4FAsqPhx5wVrvvJ2cvv1HGdtsga7O4_1-65Ad9AuXlj_bTYuA-jKsiqkrc8PbI9C27KiJhYbxQumhLUMoh7AVgrfe0VdWPxSJwYikpo8pXUgnC5G9Qmv7TxQ8-msgv_Yhl_nj0Cw_yGvszlrzkswxhTabP00JBx-c7nm91HWnjNP_mRt35iYx65yrlbE72wgE5qPpS8WJ2PHeMe5l6JXKLE2qlHEAvixXTqIt3e2VNMVQC390FE4b1soXkr4iy7MbHae714nvNLYjxFHNh0AUqc84kvJ7FYFdSVSTGVQgFzL1pEQzTWuDLbccu-x9GHdbfLtNdC7Hxf3M3CETOyItBmywTXyo0WMjcASOu-FydECxSYLbcr22SCY9AF3mo=w1078-h721-no?authuser=0">
             <section>
                 <h2>Is planting trees a good way to remove carbon dioxide from the atmosphere?</h2>
 
