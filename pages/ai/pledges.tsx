a0bdfb2bdaf1a38288f86a871b8f97e0a2e05ae9
Daniel Hartley <daniel.hartley@altruistiq.com>
2023-08-05 08:46:21 +0100

    new pledge

--- pages/ai/pledges.tsx
+++ pages/ai/pledges.tsx
@@ -93,6 +93,16 @@ const Pledges = () => {
         <p>
           <h3>Pledges</h3>
           <p>
+            <div class={styles.inset}>            
+              <div>
+                  I pledge to stop using ChatGPT until the conditions and rights of employees involved in outsourced work have been addressed.
+              </div>
+              <div>
+                <strong>Pledge checkpoint: end of August 2023</strong>                
+              </div>
+            </div>
+          </p>
+          <p>
             <div class={styles.inset}>
               <div>
                 <del>
@@ -104,11 +114,14 @@ const Pledges = () => {
                 <ins>
                   I pledge to stop using ChatGPT until I have a better understanding of its impact on resources and
                   people.
-                </ins>{' '}
+                </ins>
                 <span> [Updated 6 July 2023]</span>
               </div>
               <div>
-                <strong>Pledge checkpoint: end of July 2023</strong>
+                <strong>Pledge checkpoint: end of July 2023</strong>                
+              </div>
+              <div>
+                <em>Pledge report: pledge fulfilled</em>
               </div>
             </div>
           </p>
