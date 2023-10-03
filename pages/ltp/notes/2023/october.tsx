6a8c00c2a9cc5df6a2d76b8d8eabac736cfc2589
Daniel Hartley <daniel.hartley@altruistiq.com>
2023-10-03 20:50:04 +0100

    tpl october

---
+++ pages/ltp/notes/2023/october.tsx
@@ -0,0 +1,188 @@
+import Layout from 'components/layout/layout'
+import Links from 'components/links/Links'
+import Accordion from 'components/accordion/accordion'
+import styles from 'pages/pages.module.scss'
+
+const October = () => {
+  return (
+    <Layout
+      header="October 2023 Notes"
+      title="October 2023 Notes"
+      description={'October 2023 Notes'}
+      image="https://lh3.googleusercontent.com/xYJeSlYaHX6cb3Jv7V7HxYZDTmT6vQQT6BoLAeC-c2F8GH1O9F0Mz1x2e1KGLY1Qx8HlfCIU2GLbC4HqHwOEfWNqauMXesTRJHNRtZFPnOLX7IEPJnK7HhFjzHQ4RhwVsli_fynV-9yuZJkIyiv3TyNRNvp3S1nXZkccrszVcNzTDKlsK4DaOIXQpf6R0opkJxRQWfxRcrcCckA85H8MYwzsGD1-w-o0S6MXaIlaMH37wS1-pvnG-xWkwCUyVyFllTwXMhcFTxyRaHiuNYqfbXTfk4gfOwhgrvJHHEqch096v5nCxvguBtKgqQ0kgtobozubuKxM3-23oRrWlou2vDu5G4FAsqPhx5wVrvvJ2cvv1HGdtsga7O4_1-65Ad9AuXlj_bTYuA-jKsiqkrc8PbI9C27KiJhYbxQumhLUMoh7AVgrfe0VdWPxSJwYikpo8pXUgnC5G9Qmv7TxQ8-msgv_Yhl_nj0Cw_yGvszlrzkswxhTabP00JBx-c7nm91HWnjNP_mRt35iYx65yrlbE72wgE5qPpS8WJ2PHeMe5l6JXKLE2qlHEAvixXTqIt3e2VNMVQC390FE4b1soXkr4iy7MbHae714nvNLYjxFHNh0AUqc84kvJ7FYFdSVSTGVQgFzL1pEQzTWuDLbccu-x9GHdbfLtNdC7Hxf3M3CETOyItBmywTXyo0WMjcASOu-FydECxSYLbcr22SCY9AF3mo=w1078-h721-no?authuser=0"
+    >
+      <section>
+        <div class={styles.day}>
+          <span class={styles.date}>Sunday 01 October 2023</span>
+
+          <Links.EL link={{source:'https://news.mongabay.com/2022/04/cradle-of-transformation-the-mediterranean-and-climate-change/', label:'Cradle of transformation: The Mediterranean and climate change'}}>Cradle of transformation: The Mediterranean and climate change</Links.EL>
+          <div>John Cannon | Mongabay</div>
+
+          <blockquote>
+            <div>The Mediterranean region is warming 20% faster than the world as a whole, raising concerns about the impacts that climate change and other environmental upheaval will have on ecosystems, agriculture and the region’s 542 million people.</div>
+            <div>Heat waves, drought, extreme weather and sea-level rise are among the impacts that the region can expect to see continue through the end of the century, and failing to stop emissions of carbon dioxide and other greenhouse gases could make these issues worse.</div>
+            <div>Charting a course that both mitigates climate change and bolsters adaption to its effects is further complicated by the Mediterranean’s mix of countries, cultures and socioeconomics, leading to wide gaps in vulnerability in the region.</div>
+          </blockquote>
+
+          <ul>
+            <li>In the parts of the Mediterranean that rely on rain-fed crops, the more concentrated and intense winter rainfalls anticipated by climate forecasts are likely to leech soils of key nutrients.</li>
+            <li>Soil biodiversity, which includes everything from invisible microbes to insects and earthworms, is waning in the Mediterranean and could be pushing a critical food-producing region toward utter collapse.</li>
+            <li>The loss of soil moisture could lead to a self-perpetuating cycle of drought. With less water evaporation as temperatures continue to bake the soil, there’s less rain to replenish that moisture, leading in turn to more drought.</li>
+            <li>One way to adapt would be the introduction of drought-adapted crop varieties. That transition has already begun in parts of France’s wine industry.</li>
+            <li>Irrigation may provide stopgap relief by bringing enough water to coax a few more harvests from drier soil. But the longer-term impacts could lead to a broader drawdown of the region’s water resources, and to a buildup of salt in soils — an effect that wrecked ancient Mesopotamian civilizations.</li>
+          </ul>
+
+          <Links.EL link={{source:'https://www.medecc.org/', label:'1st Mediterranean Assessment Report'}}>1st Mediterranean Assessment Report</Links.EL>
+          <div>The Mediterranean Experts on Climate and environmental Change (MedECC)</div>
+
+          <Links.EL link={{source:'https://www.youtube.com/watch?v=fgBozLCGUHY', label:'Cradle of transformation: The Mediterranean and climate change'}}>Cradle of transformation: The Mediterranean and climate change</Links.EL>
+          <div>Ocean Acidification | Bozeman Science | YouTube</div>
+
+
+        </div>
+
+        <div class={styles.day}>
+          <span class={styles.date}>Monday 02 October 2023</span>
+
+          <h3>Mediterranean climate</h3>
+
+          <p>The Mediterranean has a subtropical climate with hot summers and mild winters. Precipitation is concentrated in the winter.</p>
+          <p>The subtropics fall outside of the tropics of Cancer (in the northern hemisphere) and Capricorn (southern hemisphere).</p>
+          <p>The tropics mark the most northerly and southerly (subsolar) points.</p>
+          <p>The Tropic of Cancer (or the Northern Tropic) is the circle of latitude that contains the subsolar point at the June (or northern) solstice when the Northern Hemisphere is tilted toward the Sun to its maximum extent.</p>
+          <p>The Tropic of Capricorn (or the Southern Tropic) is the circle of latitude that contains the subsolar point at the December (or southern) solstice when the Southern Hemisphere is tilted toward the Sun to its maximum extent.</p>
+          <p>On the June solstice the Tropic of Cancer receives more sunlight than anywhere else on earth.</p>
+          <p>On the December solstice the Tropic of Capricorn receives more sunlight than anywhere else on earth.</p>
+          <p>Mediterranean climate has a Köppen climate classification of Csa/Csb (Temperate/Semi-arid/Hot summer or Warm summer).</p>
+        </div>
+
+        <h3>Unimagined</h3>
+
+        <p>My thoughts break in two directions - towards despondency (that my archetypal landscape is artificial, a consequence of millenia of human disruption) - and towards hope (that this counterfeit is a savage-benign successor to the original).</p>
+        <p>It is perfect and imperfect, a human place, made by us for us; an example of what is best for us and a reflection of what is best in us.</p>
+        <p>We need to turn things around; not congratulate ourselves for small sacrificies. Instead we must describe how we should behave, and to reveal every step backwards for the failure that it is.</p>
+        <p>We do not like to say must; but we must. We must be clear-eyed in our observations and we must categorical in demands (both those we make of ourselves and those we make of others).</p>
+
+        <div class={styles.day}>
+          <span class={styles.date}>Monday 02 October 2023</span>
+
+          <h3>Mediterranean climate on a retrograde rotating Earth</h3>
+
+          <Links.EL link={{source:'https://esd.copernicus.org/articles/9/1191/2018/', label:'The climate of a retrograde rotating Earth'}}>The climate of a retrograde rotating Earth</Links.EL>
+          <div>Mikolajewicz et al. | European Geosciences Union</div>
+
+          <p>Wetter and stormier.</p>
+
+          <h3>Did human intervention lead to a Lost Eden, or was it a benign influence?</h3>
+
+          <Links.EL link={{source:'https://link.springer.com/article/10.1007/s10745-006-9030-4?', label:'The ‘Design’ of Mediterranean Landscapes: A Millennial Story of Humans and Ecological Systems during the Historic Period'}}>The ‘Design’ of Mediterranean Landscapes: A Millennial Story of Humans and Ecological Systems during the Historic Period</Links.EL>
+          <div>Jacques Blondel | Abstract</div>
+
+          Factors to take into account:
+          <ul>
+            <li>One cannot understand the components and dynamics of current biodiversity in the Mediterranean without taking into account the history of human-induced changes.</li>
+            <li>The various systems of land use and resource management that provided a framework for the blossoming of Mediterranean civilizations also had profound consequences on the distribution and dynamics of species, communities, and landscapes.</li>
+            <li>The processes of domestication of plant and animal species, which first occurred in the eastern Mediterranean area some 10,000 years ago, contributed to the increase of certain components of biodiversity at several spatial scales. Positive and negative feedback cycles between cultural practices and natural systems at the local and regional levels have kept ecosystems robust and resilient.</li>
+            <li>Assuming that human action can, to a certain extent, be considered a large-scale surrogate for natural sources of ecosystem disturbance, such patterns give support to the diversity-disturbance hypothesis—specifically, intermediate levels of disturbance have promoted biological diversity.</li>
+            <li>Intraspecific adaptive variation increased as a result of human-induced habitat changes over millennia, resulting in bursts of differentiation during the later Holocene of local ecotypes and gene pools of domesticated and wild plant and animal species, with region-specific characters fitting them to local climate and environmental conditions.</li>
+          </ul>
+
+          <p>It is concluded that a high degree of resilience of Mediterranean ecosystems resulted in a dynamic coexistence of human and natural living systems, which in some cases provided stability, while fostering diversity and productivity.</p>
+
+          <h3>Olive oil</h3>
+
+          <h4>Papers</h4>
+
+          <Links.EL link={{source:'https://www.mdpi.com/2076-2615/12/12/1551', label:'Impact of Super-High Density Olive Orchard Management System on Soil Free-Living and Plant-Parasitic Nematodes in Central and South Italy'}}>Impact of Super-High Density Olive Orchard Management System on Soil Free-Living and Plant-Parasitic Nematodes in Central and South Italy</Links.EL>
+          <div>Silvia Landi et al. | Italy</div>
+
+          <blockquote>
+            <div>The SHD olive orchard system may change the soil nematode community associated with olive orchards, especially concerning plant-parasitic nematodes.</div>
+            <div>The negative effects were mainly evident in stressed environments due to the dry summers and the lowest TOC content. Nevertheless, using a conservative and sustainable soil management method may maintain or improve the soil nematode community functionality and prevent the plant-parasitic nematode increase.</div>
+          </blockquote>
+
+          <Links.EL link={{source:'https://www.sciencedirect.com/science/article/abs/pii/S0308521X19310650', label:'Is new olive farming sustainable'}}>Is new olive farming sustainable?</Links.EL>
+          <div>Hanene et al. | Agricultural Systems</div>
+
+          <blockquote>
+            <div>Olive (Olea europaea L.) is a widely spread tree species in the Mediterranean.</div>
+            <div>In the last decades, olive farming has known major management changes with high economic and environmental impacts. The fast track expansion of this modern olive farming in these recent years casts doubts on the sustainability of such important tree plantation across the Mediterranean.</div>
+            <div>
+              Main olive grove types
+              <ul>
+                <li>Low density <strong>LD</strong> (100 trees ha<span class={styles.super}>-1</span>)</li>
+                <li>Medium density <strong>MD</strong> (200 trees ha<span class={styles.super}>-1</span>)</li>
+                <li>High density <strong>HD</strong> (400 trees ha<span class={styles.super}>-1</span>)</li>
+                <li>Super high density <strong>SHD</strong> (1650 trees ha<span class={styles.super}>-1</span>)</li>
+              </ul>
+              <div>10.8 Mha of olive trees are cultivated worldwide, 95% of which are in the Mediterranean region.</div>
+              <div>They help preserve natural resources by protecting the soil and sequestering carbon.</div>
+              <div>Agricultural management of olive trees has the potential to <strong>increase the accumulation of soil organic matter</strong>.</div>
+              <div>The potential of olive tree plantations to <strong>store stable organic carbon acting as CO2 sinks</strong> has been confirmed under some soil conservation practices.</div>
+              <div>Olive trees are drought-tolerant but water affects vegetative growth.</div>
+              <div>Olive trees are also limited by frost and high temperatures, and to a lesser extent by low soil fertility.</div>
+              <div>Trees in SHD orchards are usually irrigated, fertilized and trained to be suitable for mechanical harvesting and pruning, thus increasing productivity and profitability.</div>
+              <div>Paper based on studying trials on OliveCan, a process-based model of olive trees.</div>
+              <div>The rest of the paper is unavailable without payment…</div>
+            </div>
+          </blockquote>
+
+          <h4>Deficit Irrigation</h4>
+
+          <Links.EL link={{source:'https://www.sciencedirect.com/topics/agricultural-and-biological-sciences/deficit-irrigation', label:'Deficit Irrigation'}}>Science Direct papers on Deficit Irrigation</Links.EL>
+
+          <h4>Water</h4>
+
+          <Links.EL link={{source:'https://www.mdpi.com/2073-4441/15/13/2486', label:'Sustainability of High-Density Olive Orchards: Hints for Irrigation Management and Agroecological Approaches'}}>Sustainability of High-Density Olive Orchards: Hints for Irrigation Management and Agroecological Approaches</Links.EL>
+          <div>Justino Sobreiro et al. | Beja</div>
+
+          <blockquote>
+            <div>Traditionally, olive trees have been grown in the region surrounding the Mediterranean, mainly as a rainfed crop with low productivity given the typical dry environment of this region.</div>
+            <div>In recent years, the expansion of olive oil and table olive production has been achieved through both an increase in the planted area and through intensification within and beyond the Mediterranean countries by increasing the orchards’ density and via the introduction of irrigation.</div>
+            <div>In the last two decades, HD and SHD orchards, known as hedgerow olive orchards, have been developed to further reduce harvesting costs using over-the-row harvesting machines.</div>
+            <div>Current water scarcity in traditional olive-growing regions, like Alentejo, along with the expected increase in heat waves and droughts caused by climate change [16,19,20], imply an urgent need to reduce the use of water for irrigation of crops in these regions and to adopt measures to avoid the degradation of soil resources and biodiversity.</div>
+          </blockquote>
+
+          Traditional Olive Orchards (TD)
+          <ul>
+            <li>Management of cover crops is conducted by tillage or total herbicide coverage. Grain crops were traditionally grown within olives as primary sources of farmers' income. Soil erosion can be dramatic and temperature of the soil's top layer is high in the summer (over 40 °C). Although olive is a well-adapted species to drought conditions, the soil's exposure to direct sun and the lack of canopy shade over the tree root zone leads to water and heat stress, and can induce summer dormancy in the trees.</li>
+            <li>Farmers use few fertilizers and apply a reduced number of chemical pest and disease treatments in the olive groves. They are pruned every four years by chain saw, and the pruning residue is generally burned. The alternate bearing is very strong, with a sparse yield in the year following pruning. Since these orchards are rainfed, the biodiversity of species is sometimes low due to the lack of water and cover crops.</li>
+            <li>Traditionally, the harvest is performed by hand with wood sticks, although nowadays, some growers use portable backpack shakers with or without nets covering the floor.</li>
+            <li>Net production of these olive ecosystems is less than 3t ha<span class={styles.super}>-1</span> of fruits</li>
+          </ul>
+
+          Medium-Density Olive Orchards (MD)
+          <ul>
+            <li>The most common olive orchards in the Mediterranean area are those with MD (e.g. in lime soils of the southern parts of Portugal or Spain).</li>
+            <li>They are rainfed or little irrigated, and the soil is kept weed-free by tillage or by partial (in the rows) or total herbicide application.</li>
+            <li>Many have spontaneous cover plants, mainly in the interrows, which are used to some extent as grazing lands. In this case, animal manure provides some nutrient recycling for the ecosystem and complements the annual fertilization.</li>
+            <li>The pruning is carried out in alternate years and is less intense than in the traditional orchards. The pruning residue is often burned.</li>
+            <li>The sun exposure of the soil is lower due to the improved tree shade, resulting in better development of resident herbaceous vegetation that increases insect populations, improving biodiversity, and provides more protection against soil erosion than in the TD systems.</li>
+            <li>The harvest is carried out by tree shaking using floor nets or wraps around the trees as collecting systems.</li>
+            <li>These orchards have been upgraded over time by increasing plant density and providing better irrigation. This agricultural system is undergoing a fast transition to a higher-density system.</li>
+            <li>Net production of these olive ecosystems is 3-6t ha<span class={styles.super}>-1</span> of fruits</li>
+          </ul>
+
+          The High- (HD) and Super-High-Density (SHD) Olive Orchards
+          <ul>
+            <li>Net production of HD ecosystems is 6-12t ha<span class={styles.super}>-1</span> of fruits</li>
+            <li>Net production of SHD ecosystems is 12-22t ha<span class={styles.super}>-1</span> of fruits</li>
+          </ul>          
+
+          <h3>Industry articles</h3>
+
+          <h4>Commercial cultivars</h4>
+
+          <Links.EL link={{source:'https://livitaplus.com/varieta/?lang=en', label:'The best for the super high density olive farming'}}>The best for the super high density olive farming</Links.EL>
+          <div>Livita +</div>
+
+          <Links.EL link={{source:'https://www.agromillora.com/olint/en/high-density-or-sustainability/', label:'High density or sustainability?'}}>High density or sustainability?</Links.EL>
+          <div>Salvatore Campose | Agromillora</div>
+        </div>
+
+      </section>
+    </Layout>
+  )
+}
+
+export default October
