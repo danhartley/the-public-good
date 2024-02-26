f9b3604cf1d997f29b14e21e772b507a6f55540e
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-02-26 10:12:46 +0000

    new intro for ifieldnotes

---
+++ pages/ifieldnotes/introduction.tsx
@@ -0,0 +1,33 @@
+import Layout from 'components/layout/layout'
+import styles from 'pages/pages.module.scss'
+
+const Introduction = () => {
+  return (
+    <Layout
+      header="Introduction 2024 iFieldnotes.org"
+      title="Introduction 2024 iFieldnotes.org"
+      description={'Introduction 2024 iFieldnotes.org'}
+      image="https://lh3.googleusercontent.com/xYJeSlYaHX6cb3Jv7V7HxYZDTmT6vQQT6BoLAeC-c2F8GH1O9F0Mz1x2e1KGLY1Qx8HlfCIU2GLbC4HqHwOEfWNqauMXesTRJHNRtZFPnOLX7IEPJnK7HhFjzHQ4RhwVsli_fynV-9yuZJkIyiv3TyNRNvp3S1nXZkccrszVcNzTDKlsK4DaOIXQpf6R0opkJxRQWfxRcrcCckA85H8MYwzsGD1-w-o0S6MXaIlaMH37wS1-pvnG-xWkwCUyVyFllTwXMhcFTxyRaHiuNYqfbXTfk4gfOwhgrvJHHEqch096v5nCxvguBtKgqQ0kgtobozubuKxM3-23oRrWlou2vDu5G4FAsqPhx5wVrvvJ2cvv1HGdtsga7O4_1-65Ad9AuXlj_bTYuA-jKsiqkrc8PbI9C27KiJhYbxQumhLUMoh7AVgrfe0VdWPxSJwYikpo8pXUgnC5G9Qmv7TxQ8-msgv_Yhl_nj0Cw_yGvszlrzkswxhTabP00JBx-c7nm91HWnjNP_mRt35iYx65yrlbE72wgE5qPpS8WJ2PHeMe5l6JXKLE2qlHEAvixXTqIt3e2VNMVQC390FE4b1soXkr4iy7MbHae714nvNLYjxFHNh0AUqc84kvJ7FYFdSVSTGVQgFzL1pEQzTWuDLbccu-x9GHdbfLtNdC7Hxf3M3CETOyItBmywTXyo0WMjcASOu-FydECxSYLbcr22SCY9AF3mo=w1078-h721-no?authuser=0"
+    >
+      <section>
+        <div class={styles.day}>
+          <span class={styles.date}>Friday 09 2024</span>
+
+          <h3>Alpha release</h3>
+
+          <h4>Known issues</h4>
+          <ul>
+            <li>Attribution</li>
+          </ul>
+
+          <h4>Feature requests</h4>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default Introduction
