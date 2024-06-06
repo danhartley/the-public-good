b0facfe0554970182e9492c4d2a151b3668ea1c6
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-06-06 11:06:32 +0100

    update broken links discovered by https://validator.w3.org/checklink

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 
         <section>
 
-            <p>Websites do not exist in isolation. Please explore these influences: <Links.EL link={{label:'Low-tech Magazine\'s solar-powered website', source:'https://solar.lowtechmagazine.com/'}}>A solar-powered website</Links.EL>, <Links.EL link={{label:'The communal computer developed at Dynamicland', source:'https://dynamicland.org/'}}>A communal computer</Links.EL>, <Links.EL link={{source:'https://ourworldindata.org/time-use-living-conditions'}}>The world's energy problem</Links.EL>, <Links.EL link={{source:'https://neumz.com/app/'}}>Daily Gregorian Chant</Links.EL>, and <Links.EL link={{label:'Link to the YouTube video, The Origin of the Elements, which does not have synchronized captioning', source:'https://www.youtube.com/watch?v=ZJQjjBR6PbY&t=19s'}}>The Origin of the Elements</Links.EL>.</p>
+            <p>Websites do not exist in isolation. Please explore these influences: <Links.EL link={{label:'Low-tech Magazine\'s solar-powered website', source:'https://solar.lowtechmagazine.com/'}}>A solar-powered website</Links.EL>, <Links.EL link={{label:'The communal computer developed at Dynamicland', source:'https://dynamicland.org/'}}>A communal computer</Links.EL>, <Links.EL link={{source:'https://ourworldindata.org/time-use'}}>The world's energy problem</Links.EL>, <Links.EL link={{source:'https://neumz.com/app/'}}>Daily Gregorian Chant</Links.EL>, and <Links.EL link={{label:'Link to the YouTube video, The Origin of the Elements, which does not have synchronized captioning', source:'https://www.youtube.com/watch?v=ZJQjjBR6PbY&t=19s'}}>The Origin of the Elements</Links.EL>.</p>
 
             <p>Our ability to mitigate the threats we pose to the planet depends on individual and collective action.</p>
 
@@ -52,22 +52,22 @@ export default function Home() {
 
             <h3>Performance</h3>
             <p>Having a <Links.EL link={{label:'Mozilla Internet Health Report 2019 states that the slowest mobile broadband internet in the world is also the least affordable', source:'https://internethealthreport.org/2019/the-worlds-slowest-internet-is-the-least-affordable/'}}>slow connection</Links.EL> is not unusual.</p>
-            <p>Data can be expensive, <Links.EL link={{label:'Our World in Data map showing that the cost of 1GB of mobile data as a percentage of income, is highest a spread of African countries', source:'https://ourworldindata.org/grapher/cost-of-1gb-of-mobile-data?region=World'}}>especially in countries where only mobile connectivity is available</Links.EL>.</p>
-            <p>Poor design <Links.EL link={{label:'WebKit guidelines for developers on how to minimise mobile and laptop energy use', source:'https://webkit.org/blog/8970/how-web-content-can-affect-power-usage/'}}>runs down batteries</Links.EL>, and too many websites are <Links.EL link={{source:'https://www.nngroup.com/articles/the-need-for-speed'}}>too slow</Links.EL>.</p>
+            <p>Data can be expensive, <Links.EL link={{label:'cable.co.uk: The cost of 1GB of mobile data in 237 countries', source:'https://www.cable.co.uk/mobiles/worldwide-data-pricing/'}}>especially in countries where only mobile connectivity is available</Links.EL>.</p>
+            <p>Poor design <Links.EL link={{label:'WebKit guidelines for developers on how to minimise mobile and laptop energy use', source:'https://webkit.org/blog/8970/how-web-content-can-affect-power-usage/'}}>runs down batteries</Links.EL>, and too many websites are <Links.EL link={{source:'https://www.nngroup.com/articles/the-need-for-speed/'}}>too slow</Links.EL>.</p>
             <p>When you waste people's time, <Links.EL link={{source:'https://developer.mozilla.org/en-US/docs/Learn/Performance/why_web_performance'}}>they don't hang around</Links.EL>.</p>
             <p><Links.EL link={{source:'https://www.nngroup.com/articles/powers-of-10-time-scales-in-ux/'}}>It takes 0.05 seconds to form an opinion</Links.EL>.</p>
 
             <p><Links.IL link={{source:'performance'}}>Read more about performance</Links.IL></p>
 
             <h3>Accessibility</h3>
-            <p>Using websites can be challenging <Links.EL link={{source:'https://developers.google.com/web/fundamentals/accessibility'}}>for many reasons</Links.EL>.</p>
-            <p><Links.EL link={{source:'https://web.dev/load-faster-like-proxx/'}}>Not all phones are smart</Links.EL>.</p>
+            <p>Using websites can be challenging <Links.EL link={{source:'https://web.dev/articles/accessibility'}}>for many reasons</Links.EL>.</p>
+            <p><Links.EL link={{source:'https://web.dev/articles/load-faster-like-proxx'}}>Not all phones are smart</Links.EL>.</p>
             
             <p><Links.IL link={{source:'accessibility'}}>Read more about accessibility</Links.IL></p>
 
             <h3>Sustainability</h3>
             <p>
-                <div>There is a <Links.EL link={{source:'https://www.carbonbrief.org/factcheck-what-is-the-carbon-footprint-of-streaming-video-on-netflix'}}>cost in greenhouse gas emissions</Links.EL> for every page.</div>
+                <div>There is a <Links.EL link={{source:'https://www.carbonbrief.org/factcheck-what-is-the-carbon-footprint-of-streaming-video-on-netflix/'}}>cost in greenhouse gas emissions</Links.EL> for every page.</div>
             </p>
 
             <p><Links.IL link={{source:'sustainability'}}>Read more about sustainability</Links.IL></p>
