926b8be4bae95935726f9aef20a948f45c4b492d
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-07-17 16:42:07 +0100

    remove deleted svg refs in ignore

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,7 +21,7 @@ class MyDocument extends Document {
           <NextScript />
           <script async defer src="https://scripts.withcabin.com/hello.js"></script>
           {/* <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-          <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="danhartley" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pt.linkedin.com/in/danhartley?trk=profile-badge">Daniel Hartley</a></div>               */}
+          <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="danhartley" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pt.linkedin.com/in/danhartley?trk=profile-badge">Daniel Hartley</a></div>               */}          
         </body>
       </Html>
     )
