0690f8efe2e647a3295879bc8516f7aea69da521
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-07-18 10:30:37 +0100

    pass url instead of domain

--- components/emissions/emissions.tsx
+++ components/emissions/emissions.tsx
@@ -9,10 +9,10 @@ const Emissions = () => {
     (async () => {      
       await browser.registerServiceWorker()
     
-      const domain = window.location.host
-      const { pageWeight, count, greenHosting, emissions, mgCO2 } = await browser.getPageEmissions({domain})
+      const url = window.location.origin
+      const { pageWeight, count, greenHosting, emissions, mgCO2 } = await browser.getPageEmissions(url)
       
-      console.log(`Report for ${domain}`)
+      console.log(`Report for ${url}`)
       console.log('Page weight: ', `${pageWeight / 1000} Kbs`)
       console.log('Requests ', count)  
       console.log('Emissions: ', `${mgCO2} mg of CO2`)
