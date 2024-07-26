62f652a2c9f6b4c35923438aca2254f62ea7edde
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-07-26 10:57:01 +0100

    update emissions output to reflect changes to emissions api

--- components/emissions/emissions.tsx
+++ components/emissions/emissions.tsx
@@ -17,19 +17,15 @@ const Emissions = () => {
         }
       }
 
-      const { pageWeight, count, greenHosting, emissions, mgCO2, records } = await browser.getPageEmissions(url, options)
+      const { bytes, count, greenHosting, emissions, mgCO2, data } = await browser.getPageEmissions(url, options)
       
       console.log(`Report for ${url}`)
-      console.log('Page weight: ', `${pageWeight / 1000} Kbs`)
+      console.log('Page weight: ', `${bytes / 1000} Kbs`)
       console.log('Requests ', count)  
       console.log('Emissions: ', `${mgCO2} mg of CO2`)
       console.log(greenHosting ? 'Hosting: green hosting' : 'Hosting: not green hosting')
-      console.log('Records ', records)
+      console.log('Data ', data)
 
-      const bytes = records.reduce((acc, curr) => acc + curr.responseBytes, 0)
-
-      console.log('bytes: ', bytes)
-  
       await browser.clearPageEmissions()
     })() 
   })
