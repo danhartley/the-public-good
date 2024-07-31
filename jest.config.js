5073f42879043b69f6dab3f85fe63ae1445aa018
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-07-31 10:59:17 +0100

    remove eslint packages and configs and update jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     preset: "ts-jest",
     testRegex: '(/__tests__/.*|(\\.|/)(test))\\.tsx?$',
-    moduleDirectories: ['node_modules', './']
+    moduleDirectories: ['node_modules', '<rootDir>']
   };
